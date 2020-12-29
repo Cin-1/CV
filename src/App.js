3f5ee2408f6dbb00fc6a7549904231c33209d0a0
Lirico <dominguezmatiasadrian@gmail.com>
2020-12-28 23:56:17 -0300

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Fragment } from "react";
 import Form from "./Components/Contact/Form";
 import Navegation from "./Components/layout/Navegation";
+import Home from "./Components/layout/Home";
 
 function App() {
   return (
     <Fragment>
 
       <Navegation/>
-
+      <br/>
+      <Home/>
+      <br/>
       <Form />
 
-      </Fragment>
+    </Fragment>
   );
 }
 
