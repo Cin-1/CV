54eedf18d54bdf402ba71b2098837653ab72f24d
Cinthia Daira Pardos <cin@pop-os.localdomain>
2020-12-29 15:58:35 -0300

    forms

--- src/Components/Contact/Form.js
+++ src/Components/Contact/Form.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
 import useForm from "./useForm";
 import validate from "./validate";
+import Swal from "sweetalert2";
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: "start-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
 const Form = () => {
+  const [isSent, setIsSent] = useState(false);
   const submit = () => {
     console.log("enviando....");
+    fetch(`https://hooks.zapier.com/hooks/catch/9191970/ocq863q/`, {
+      method: "POST",
+      body: JSON.stringify({ email, comment, key: feature }).then(() => setIsSent(true))};
+    Toast.fire({
+      icon: "success",
+      title: "Se enviaron los datos. Muchas gracias!",
+    });
   };
   const { values, handleChange, handleSubmit, errors } = useForm(
     submit,
     validate
   );
 
+  const email = values.email;
+  const comment = values.msj;
   return (
     <div>
       <h1>Form Contacto</h1>
