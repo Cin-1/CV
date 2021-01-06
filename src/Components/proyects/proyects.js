0721e01a9d7760f18202d9775703bc8462a87ecd
Cinthia Daira Pardos <cin@pop-os.localdomain>
2021-01-05 22:17:59 -0300

    carousel

--- src/Components/proyects/proyects.js
+++ src/Components/proyects/proyects.js
@@ -1,11 +1,30 @@
 import React, { Fragment } from "react";
 import Imagen from "../../images/eccomerce.png";
 import styled from "@emotion/styled";
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext,
+  ButtonFirst,
+  ButtonLast,
+} from "pure-react-carousel";
+import "pure-react-carousel/dist/react-carousel.es.css";
 
 const DivP = styled.div`
   background-color: white;
   padding-bottom: 5%;
   padding-top: 5%;
+`;
+const DivBotton = styled.div`
+  position: relative;
+  width: 80%;
+  left: 0;
+  text-align: center;
+  margin: auto;
+  margin-bottom: -20px;
+  z-index: 100;
 `;
 
 const Title = styled.h1`
@@ -44,32 +63,38 @@ const Title = styled.h1`
 const DivFlex = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-around;
+  justify-content: center;
   font-family: "Ubuntu", sans-serif;
+  background-color: beige;
 `;
 const SubTitle = styled.h3`
   font-family: "Ubuntu", sans-serif;
   color: black;
   text-align: center;
-  margin: 2%;
 `;
 
 const DivImg = styled.div`
-  margin: 3%;
-  background-color: white;
+  margin: 2%;
+  background-color: rgb(215, 215, 215);
+  max-width: 90%fit-content;
   align-items: center;
   padding: 3%;
   border-radius: 2rem;
   &:hover {
     background-color: crimson;
     cursor: pointer;
+    transform: scale(1.03);
   }
 `;
 
 const Img = styled.img`
   max-width: 100%;
   border-radius: 2rem;
-  border: black 2px solid;
+  border: black 3px solid;
+  object-fit: cover;
+  &:hover {
+    border-color: white;
+  }
 `;
 const P = styled.p`
   font-family: "Ubuntu", sans-serif;
@@ -83,23 +108,84 @@ const Proyects = () => {
     <>
       <DivP>
         <Title>Mis proyectos</Title>
-        <DivFlex>
-          <DivImg>
-            <SubTitle>Ecommerce</SubTitle>
-            <Img src={Imagen} alt="imagen ecommerce" />
-            <P>Descripcion del proyecto</P>
-          </DivImg>
-          <DivImg>
-            <SubTitle>Ecommerce</SubTitle>
-            <Img src={Imagen} alt="imagen ecommerce" />
-            <P>Descripcion del proyecto</P>
-          </DivImg>
-          <DivImg>
-            <SubTitle>Ecommerce</SubTitle>
-            <Img src={Imagen} alt="imagen ecommerce" />
-            <P>Descripcion del proyecto</P>
-          </DivImg>
-        </DivFlex>
+        <CarouselProvider
+          naturalSlideWidth={100}
+          naturalSlideHeight={125}
+          totalSlides={2}
+        >
+          <DivBotton>
+            <ButtonBack
+              style={{
+                height: "13px",
+                width: "13px",
+                margin: "0 5px",
+                outline: "none !important",
+                border: " 5px crimson",
+                borderRadius: "50%",
+                backgroundColor: "black",
+              }}
+            ></ButtonBack>
+            <ButtonNext
+              style={{
+                height: "13px",
+                width: "13px",
+                margin: "0 5px",
+                outline: "none !important",
+                border: "crimson",
+                borderRadius: "50%",
+                color: "red",
+                backgroundColor: "black",
+              }}
+            ></ButtonNext>
+          </DivBotton>
+          <div>
+            <Slider>
+              <Slide index={0}>
+                <DivFlex>
+                  <DivImg>
+                    <a href="https://www.facebook.com/">
+                      <SubTitle>Ecommerce</SubTitle>
+                      <Img src={Imagen} alt="imagen ecommerce" />
+                      <P>Descripcion del proyecto</P>
+                    </a>
+                  </DivImg>
+                  <DivImg>
+                    <SubTitle>Ecommerce</SubTitle>
+                    <Img src={Imagen} alt="imagen ecommerce" />
+                    <P>Descripcion del proyecto</P>
+                  </DivImg>
+                  <DivImg>
+                    <SubTitle>Ecommerce</SubTitle>
+                    <Img src={Imagen} alt="imagen ecommerce" />
+                    <P>Descripcion del proyecto</P>
+                  </DivImg>
+                </DivFlex>
+              </Slide>
+              <Slide index={1}>
+                {" "}
+                <DivFlex>
+                  <DivImg>
+                    <a href="https://www.facebook.com/">
+                      <SubTitle>Ecommerce</SubTitle>
+                      <Img src={Imagen} alt="imagen ecommerce" />
+                      <P>Descripcion del proyecto</P>
+                    </a>
+                  </DivImg>
+                  <DivImg>
+                    <SubTitle>Ecommerce</SubTitle>
+                    <Img src={Imagen} alt="imagen ecommerce" />
+                    <P>Descripcion del proyecto</P>
+                  </DivImg>
+                  <DivImg>
+                    <SubTitle>Ecommerce</SubTitle>
+                    <Img src={Imagen} alt="imagen ecommerce" />
+                    <P>Descripcion del proyecto</P>
+                  </DivImg>
+                </DivFlex>
+              </Slide>
+            </Slider>
+          </div>
+        </CarouselProvider>
       </DivP>
     </>
   );
