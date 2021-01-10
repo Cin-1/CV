3fbdeedde2c38f9ad3e28b9401f8be14b7912dac
Lirico <dominguezmatiasadrian@gmail.com>
2021-01-10 17:05:48 -0300

    commit 9

--- src/Components/layout/AboutMe.js
+++ src/Components/layout/AboutMe.js
@@ -37,10 +37,12 @@ const AboutTitle = styled.h2`
 const AboutGrid = styled.div`
     display: grid;
     grid-template-columns: 50% 50%;
-`;
 
-const AboutColumnLeft = styled.div`
-
+    @media screen and (max-width: 1024px) {
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+    }
 `;
 
 const AboutImg = styled.img`
@@ -49,6 +51,56 @@ const AboutImg = styled.img`
     object-fit: cover;
     margin-left: 100px;
     border-radius: 6px;
+
+    @media screen and (max-width: 1024px) {
+        margin-left: 0;
+        margin-bottom: -50px;
+    }
+`;
+
+const AboutColumnRight = styled.div`
+    margin-top: 15%;
+    width: 85%;
+
+    @media screen and (max-width: 1024px) {
+        margin-bottom: 50px;
+    }
+`;
+
+const CRTtitle = styled.p`
+    font-size: 25px;
+    font-weight: 600px;
+    margin-bottom: 10px;
+    font-family: 'Ubuntu', sans-serif;
+`;
+
+const CRSpan = styled.span`
+    color: crimson;
+`;
+
+const CRText = styled.p`
+    font-size: 16px;
+    font-family: 'Ubuntu', sans-serif;
+    text-align: justify;
+`;
+
+const CRBotton = styled.a`
+    display: inline-block;
+    background: crimson;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 500;
+    padding: 10px 30px;
+    margin-top: 20px;
+    border-radius: 6px;
+    border: solid 2px crimson;
+    transition: all 0.3s ease;
+
+    :hover {
+        color: crimson;
+        background: none;
+    }
+
 `;
 
 const AboutMe = () => {
@@ -57,15 +109,15 @@ const AboutMe = () => {
             <div>
                 <AboutTitle>Conóceme</AboutTitle>
                 <AboutGrid>
-                    <AboutColumnLeft>
-                        <AboutImg src="https://www.onepiecepodcast.com/wp-content/uploads/2016/09/3332296-2395105387-24698.png" alt="" />
-                    </AboutColumnLeft>
                     <div>
-                        <div><p>Soy Luffy y soy un <span>Pirata</span></p></div>
-                        <p>Lorem ipsum dolor sit amet consectetur adipiscing, elit purus facilisis proin litora tempor arcu, 
-                        tortor nisl habitant interdum id.</p>
-                        <a href="#">Descargar CV</a>
+                        <AboutImg src="https://www.onepiecepodcast.com/wp-content/uploads/2016/09/3332296-2395105387-24698.png" alt="" />
                     </div>
+                    <AboutColumnRight>
+                        <div><CRTtitle>Soy Luffy y soy un <CRSpan>Pirata</CRSpan></CRTtitle></div>
+                        <CRText>Lorem ipsum dolor sit amet consectetur adipiscing, elit purus facilisis proin litora tempor arcu, 
+                        tortor nisl habitant interdum id.</CRText>
+                        <CRBotton href="#">Descargar CV</CRBotton>
+                    </AboutColumnRight>
                 </AboutGrid>
             </div>
         </section>
